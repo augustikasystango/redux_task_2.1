b96a695c994e693abb4c387ff7c629eb31569ad8
Augustika Pal <augustika.pal@systango.com>
2025-04-14 15:26:56 +0530

    handling http req & feedbacks

--- src/App.js
+++ src/App.js
@@ -4,33 +4,61 @@ import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import axios from 'axios';
-
+import { useDispatch } from 'react-redux';
+import { toggleAction } from './store/uislice';
+import Notification from './components/UI/Notification';
 
 
 function App() {
 
+  const dispatch = useDispatch();
+
   const toggle = useSelector((state) => state.toggle.showCart)
   const cart = useSelector((state) => state.cart);
+  const notification= useSelector((state)=>state.toggle.notification)
 
   const fetchCart = async () => {
+    dispatch(toggleAction.showNotification({
+      status:'Pending',
+      title : 'Sending..',
+      messsage:'Sending cart data!'
+    }))
     try {
+      
       const data = await axios.put(`https://my-project-d40ec-default-rtdb.firebaseio.com/cart.json
 `, cart);
+dispatch(toggleAction.showNotification({
+  status:'success',
+  title : 'Success',
+  messsage:'Sent cart data!'
+}))
+
     } catch (error) {
+      dispatch(toggleAction.showNotification({
+        status:'error',
+        title : 'Error..',
+        messsage:'Oops ! , Sending cart data failed!'
+      }))
       console.log(`Error fetching cart details`, error)
     }
 
   }
   useEffect(() => {
+
+ 
     fetchCart();
-  }, [cart]);
+  }, [cart,dispatch]);
 
 
   return (
-    <Layout>
+    <>
+   {notification && <Notification status = {notification.status} message={notification.message} title={notification.title}/>}
+       <Layout>
       {toggle && <Cart />}
       <Products />
     </Layout>
+    </>
+   
   );
 }
 
--- src/store/uislice.js
+++ src/store/uislice.js
@@ -1,12 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = { showCart : true}
+const initialState = { showCart : true,notification:null}
 const toggleSlice = createSlice({
     name:'toggle',
     initialState,
     reducers:{
        toggleCart(state){
             state.showCart= !state.showCart
+       },
+       showNotification(state,action){
+            state.notification = {status : action.payload.status , title : action.payload.title,
+                message : action.payload.message
+            }
        }
     }
 })
